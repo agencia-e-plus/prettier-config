48ec2ecee5d1906fd96073611f6b976380c5ba8c
Bruno Rodrigues <bruno@agenciaeplus.com.br>
2023-02-11 02:12:21 -0300

    Initial commit 🎉

---
+++ index.js
@@ -0,0 +1,12 @@
+// Reference: https://prettier.io/docs/en/options.html
+module.exports = {
+	$schema: 'http://json.schemastore.org/prettierrc',
+	printWidth: 80,
+	tabWidth: 2,
+	singleQuote: true,
+	trailingComma: 'all',
+	arrowParens: 'always',
+	bracketSpacing: true,
+	semi: false,
+	endOfLine: 'auto'
+}
